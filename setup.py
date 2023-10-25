3412117e297ef0e42cb715bd432bacb5947b0166
wuhui2023 <18896724614@139.com>
2023-10-25 11:26:04 +0800

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-bordercontainer",
-    version="0.1.0",
+    version="0.0.1",
     author="wuhui",
     author_email="18896724614@139.com",
     description="a box which can set style easily",
